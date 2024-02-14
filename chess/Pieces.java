6cc117ad8ddb1aa6ecbe3df373c27fa16657201e
awd46 <awd46@scarletmail.rutgers.edu>
2024-02-13 21:36:27 -0500

    Subclasses

--- chess/Pieces.java
+++ chess/Pieces.java
@@ -3,3 +3,22 @@ package chess;
 public class Pieces {
     
 }
+
+class Pawn extends Pieces{
+
+}
+class Knight extends Pieces{
+
+}
+class Rook extends Pieces{
+
+}
+class Bishop extends Pieces{
+
+}
+class King extends Pieces{
+
+}
+class Queen extends Pieces{
+
+}
