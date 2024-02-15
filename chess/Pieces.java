c515edb08c57db0e747ac9055a198e37df17ca24
awd46 <awd46@scarletmail.rutgers.edu>
2024-02-14 20:55:18 -0500

    2.14 end

--- chess/Pieces.java
+++ chess/Pieces.java
@@ -2,27 +2,61 @@ package chess;
 
 public class Pieces {
 
-    public int tileCoordinate;
-    public Player color;
+    protected final int tileCoordinate;
+    protected final Player color;
     
+    Pieces(final int tileCoordinate, final Player color){
+        this.tileCoordinate = tileCoordinate;
+        this.color = color;
+    }
 
 }
 
 class Pawn extends Pieces{
 
+    Pawn(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
+
 }
 class Knight extends Pieces{
+
+    Knight(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
 
 }
 class Rook extends Pieces{
 
+    Rook(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
+
 }
 class Bishop extends Pieces{
+
+    Bishop(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
 
 }
 class King extends Pieces{
 
+    King(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
+
 }
 class Queen extends Pieces{
+
+    Queen(int tileCoordinate, Player color) {
+        super(tileCoordinate, color);
+        //TODO Auto-generated constructor stub
+    }
 
 }
