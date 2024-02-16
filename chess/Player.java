2dbf450a0f3a9c54f293471aea65918431674604
Alex Dodson <146151710+AmissGiant@users.noreply.github.com>
2024-02-15 23:59:13 -0500

    Update 2/15 1

--- chess/Player.java
+++ chess/Player.java
@@ -2,4 +2,14 @@ package chess;
 
 public class Player {
     
+    private char teamColor;
+
+    public Player(char teamColor){
+        this.teamColor = teamColor;
+    }
+
+    public char getColor(){
+        return teamColor;
+    }
+    
 }
