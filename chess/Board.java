8798bc7c2fcf29a3a85d9a1ed81a18f63724c25d
Alex Dodson <146151710+AmissGiant@users.noreply.github.com>
2024-02-16 00:23:18 -0500

    2/15 end

--- chess/Board.java
+++ chess/Board.java
@@ -2,12 +2,42 @@ package chess;
 
 public class Board {
     private final Tile[][] tiles;
+    private final Player whitePlayer;
+    private final Player blackPlayer;
 
-    public Board(){
+    public Board(Player whitePlayer, Player blackPlayer){
+        this.whitePlayer = whitePlayer;
+        this.blackPlayer = blackPlayer;
         tiles = new Tile[8][8];
         initializeBoard();
     }
+
     private void initializeBoard(){
         //method sets pieces in their starting positions
+        for(int file = 0; file < 8; file++){
+            tiles[1][file] = new OccupiedTile(file + 1, new Pawn(whitePlayer));
+            //place the rest of white pieces
+        }
+        for(itn file = 0; file < 8; file++){
+            tiles[6][file] = new OccupiedTile(file + 1, new Pawn(blackPlayer))
+            //place the rest of the black pieces
+        }
+        //fill in empty tiles
+        for(int rank = 2; rank < 6; rank++){
+            for(int file = 0; file < 8; file++){
+                tile[rank][file] = newEmptyTile(rank * 8 + (file + 1));
+            }
+        }
     }
+
+    public Tile getTile(int file, int rank){
+        //get a tile based on its rank and file
+        return tiles[rank - 1][file - 'a'];
+    }
+
+    public void setTile(int file, int rank, Tile tile){
+        //set a tile based on its file and rank
+        tiles[rank - 1][files - 'a'] = tile;
+    }
+
 }
--- chess/Coordinates.java
+++ chess/Coordinates.java
@@ -4,9 +4,9 @@ public class Coordinates {
 
 	public static int parseSquare(String square){
 		//convert chess notation to square number 1-64
-		int file = square.charAt(0) - 'a' +1;
+		int file = square.charAt(0) - 'a' + 1;
 		int rank = Character.getNumericValue(square.charAt(1));
-		return(8 - rank) * 8 + file;
+		return (8 - rank) * 8 + file;
 	}
 
     public static int[] parseMove(String move){
