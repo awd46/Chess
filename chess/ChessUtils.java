d456adf8d724c44dd6efe0cd1dbb97c284fb5234
awd46 <awd46@scarletmail.rutgers.edu>
2024-02-22 22:28:46 -0500

    2/22 final

---
+++ chess/ChessUtils.java
@@ -0,0 +1,40 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class ChessUtils {
+    public static ReturnPiece.PieceType getPieceType(Piece piece, Player player){
+        if(piece instanceof Pawn){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }else if(piece instanceof Rook){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }else if(piece instanceof Knight){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }else if(piece instanceof Bishop){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }else if(piece instanceof Queen){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }else if(piece instanceof King){
+            return piece.getColor() == player ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        }
+        return null;
+    }
+
+    public static ArrayList<ReturnPiece> getPiecesOnBoard(Board board, Player player){
+        ArrayList<ReturnPiece> pieces = new ArrayList<>();
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                Tile tile = board.getTile(i * 8 + j + 1);
+                if(tile.isOccupied()){
+                    Piece piece = tile.getPiece();
+                    ReturnPiece returnPiece = new ReturnPiece();
+                    returnPiece.pieceType = getPieceType(piece, player);
+                    returnPiece.pieceFile = ReturnPiece.PieceFile.values()[j];
+                    returnPiece.pieceRank = i + 1;
+                    pieces.add(returnPiece);
+                }
+            }
+        }
+        return pieces;
+    }
+}
